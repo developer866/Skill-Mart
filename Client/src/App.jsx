4f3eb54f823b9396be03bb3dd887363a956f5e54
Developer866 <opeyemijoseph866@gmail.com>
2025-09-09 01:29:15 +0100

    Remove commented-out div in App component for cleaner code

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,6 +8,9 @@ function App() {
       {/* container */}
       <div className="max-w-[80%] mx-auto my-4 px-2 md:px-12 border border-dotted h-[80vh]">
       </div>
+
+      {/* <div className="max-w-[80%] mx-auto my-4 px-2 md:px-12 border border-dotted h-[80vh]">
+      </div> */}
       <Footer />
     </main>
 
